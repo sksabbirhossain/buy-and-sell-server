f80e5d1ed3f5bc4af46094d5948d344cb8345cbd
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-11-24 16:08:18 +0600

    created a express server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 5000;
+
+// database connection
+// dbConnect();
+
+//route setup
+app.use("/api", (req, res) => {
+  res.send("hello api");
+});
+
+//listen server
+app.listen(port, () => {
+  console.log(`listening port ${port}`);
+});
