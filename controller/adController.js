352a67c5804567a52742488deb4707007384e3dc
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-11-27 16:47:21 +0600

    get all advertise from database

--- controller/adController.js
+++ controller/adController.js
@@ -1,4 +1,33 @@
-const { advertiseCollection } = require("../database/databaseConnection");
+const {
+  advertiseCollection,
+  productCollection,
+} = require("../database/databaseConnection");
+const { addProduct } = require("./productController");
+
+//get Advertise
+async function getAdvertise(req, res, next) {
+  try {
+    const query = {
+      status: "available",
+    };
+    const products = await productCollection.find(query).toArray();
+    const advertises = await advertiseCollection.find({}).toArray();
+    let adProducts = [];
+    advertises.map((ad) => {
+      const product = products.filter((product) => product._id == ad.productId);
+      adProducts.push(product);
+    });
+    res.send({
+      success: true,
+      data: adProducts,
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      error: err.message,
+    });
+  }
+}
 
 //post a asvertise
 async function addAdvertise(req, res, next) {
@@ -19,4 +48,5 @@ async function addAdvertise(req, res, next) {
 
 module.exports = {
   addAdvertise,
+  getAdvertise,
 };
--- router/adRouter.js
+++ router/adRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { addAdvertise } = require("../controller/adController");
+const { addAdvertise, getAdvertise } = require("../controller/adController");
 
 const router = express.Router();
 
+router.get("/my-product/get-advertise", getAdvertise);
 router.post("/my-product/advertise", addAdvertise);
 
 module.exports = router;
